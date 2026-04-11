0b6d062a2226168586c8f47594cc4c5a708127b7
PhDuy2005 <kduy1706@gmail.com>
2026-04-12 01:47:47 +0700

    update api docs. Testing plan will be created in issue #1

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,8 @@ dependencies {
 	testCompileOnly("org.projectlombok:lombok")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	testAnnotationProcessor("org.projectlombok:lombok")
+
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.14") // OpenAPI/Swagger
 }
 
 tasks.withType<Test> {
